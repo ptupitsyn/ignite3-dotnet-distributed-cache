59b49885166ad3d8863824b66b8454cf6284ff2e
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-11-25 14:39:41 +0200

    Initial commit

---
+++ IgniteDistribCaching/Program.cs
@@ -0,0 +1,59 @@
+using System.Text.Json;
+using Apache.Extensions.Caching.Ignite;
+using Apache.Ignite;
+using Microsoft.Extensions.Caching.Distributed;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddIgniteClientGroup(new IgniteClientGroupConfiguration
+    {
+        ClientConfiguration = new IgniteClientConfiguration("localhost")
+    })
+    .AddIgniteDistributedCache(options =>
+        // Table is created automatically if it does not exist
+        options.TableName = "ASPNET_DISTRIBUTED_CACHE");
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/weatherforecast",  async (IDistributedCache cache) =>
+    {
+        const string cacheKey = "weather_forecast";
+
+        byte[]? cachedData = await cache.GetAsync(cacheKey);
+        if (cachedData != null)
+        {
+            return JsonSerializer.Deserialize<IList<WeatherForecast>>(cachedData);
+        }
+
+        IList<WeatherForecast> forecast = FetchForecast();
+
+        cachedData = JsonSerializer.SerializeToUtf8Bytes(forecast);
+        await cache.SetAsync(cacheKey, cachedData, CancellationToken.None);
+
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.Run();
+
+IList<WeatherForecast> FetchForecast()
+{
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    return Enumerable.Range(1, 5).Select(GetForecast).ToArray();
+
+    WeatherForecast GetForecast(int index) =>
+        new(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        );
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
